612ae0514a6c1fb3134112d35b45fe886cd7e992
adityaprakash-work <adityaprakash.work@gmail.com>
2023-12-23 05:15:42 +0530

    __init__ patches

---
+++ dreamwalker/pytorch_generative/models/autoregressive/__init__.py
@@ -0,0 +1,9 @@
+from . import (
+    fvbn,
+    gated_pixel_cnn,
+    image_gpt,
+    made,
+    nade,
+    pixel_cnn,
+    pixel_snail,
+)
---
+++ dreamwalker/pytorch_generative/models/vae/__init__.py
@@ -0,0 +1,8 @@
+from . import (
+    beta_vae,
+    vae,
+    vaes,
+    vd_vae,
+    vq_vae,
+    vq_vae_2,
+)
